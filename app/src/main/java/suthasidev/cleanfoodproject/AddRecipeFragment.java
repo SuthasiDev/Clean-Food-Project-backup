90a0f58408ffcffe457bbc24d6eeb0afbee17c8f
SUTHASIDEV <suthasidev@gmail.com>
2017-10-06 11:57:29 +0700

    backend

---
+++ app/src/main/java/suthasidev/cleanfoodproject/AddRecipeFragment.java
@@ -0,0 +1,18 @@
+package suthasidev.cleanfoodproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddRecipeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_recipe, container, false);
+        return view;
+    }
+}
